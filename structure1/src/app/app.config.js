49b167276f3be39c604ae86e1d3021eca6064388
MonaliAkashModi <monalipatel612@gmail.com>
2018-03-23 16:26:43 +0530

    first commit

---
+++ structure1/src/app/app.config.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    function appConfig($stateProvider) {
+        $stateProvider
+            .state('parentState', {
+                template: '<div ui-view></div>' 
+            })
+            .state('parentState.childState', {
+                url: "/layout",
+                templateUrl:'app/layout.html'
+            })
+    }
+    angular.module("learning")
+        .config(appConfig);
+})();
---
+++ structure1/src/app/app.module.js
@@ -0,0 +1,5 @@
+﻿(function () {
+    angular.module("learning",[
+       "ui.router"
+    ]);
+})();
